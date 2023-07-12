e03717bafd1247bf34bc40d57ffa6682caf73d47
PatrickNulla <57047898+PatrickNulla@users.noreply.github.com>
2023-07-12 12:04:05 +0800

    Test Generic Converter

--- convert.py
+++ convert.py
@@ -3,17 +3,22 @@ import json
 import time
 from enum import Enum
 
+
 class Converter:
     class _WriteMode(Enum):
         Overwrite = 1
         CreateNew = 2
 
+    class _ConverterType(Enum):
+        Generic = 0
+        Local_Settings_FunctionApp = 1
+        Azure_DevOps_Release_Pipeline_FunctionApp = 2
+        Azure_FunctionApp = 3
+
     def __init__(self):
         with open("converter.json") as config_file:
             self.config_data = json.load(config_file)
         self.folder_name = self.config_data["folderName"]
-        self.pipeline_folder_name = self.config_data["pipelineFolderName"]
-        self.function_app_folder_name = self.config_data["functionAppFolderName"]
         self.write_mode = self.config_data["writeMode"]
         self.is_sorted = self.config_data.get("isSorted", False)
         self.reversed_sort = self.config_data.get("reversedSort", False)
@@ -21,60 +26,56 @@ class Converter:
         self.variables = self.config_data["variables"]
         self.ignore_variables = self.config_data["ignoreVariables"]
 
-    def local_to_pipeline(self):
-        self.__process_files(
-            self.__get_output_folder_path_local_to_pipeline,
-            ".txt",
-            self.__convert_local_to_pipeline,
-        )
-
-    def pipeline_to_azure_fa_config(self):
-        self.__process_files(
-            self.__get_output_folder_path_pipeline_to_azure_fa,
-            ".json",
-            self.__convert_pipeline_to_azure_fa_config,
-        )
-
-    def __get_output_folder_path_local_to_pipeline(self, environment):
-        if self._WriteMode[self.write_mode] == self._WriteMode.CreateNew:
-            timestamp = str(int(time.time()))
-            return f"{self.folder_name}_{timestamp}/{environment}_environment/{self.pipeline_folder_name}"
-        else:
-            return f"{self.folder_name}/{environment}_environment/{self.pipeline_folder_name}"
-
-    def __get_output_folder_path_pipeline_to_azure_fa(self, environment):
-        if self._WriteMode[self.write_mode] == self._WriteMode.CreateNew:
-            timestamp = str(int(time.time()))
-            return f"{self.folder_name}_{timestamp}/{environment}_environment/{self.function_app_folder_name}"
-        else:
-            return f"{self.folder_name}/{environment}_environment/{self.function_app_folder_name}"
-
-    def __process_files(self, get_output_folder_path_func, file_extension, convert_func):
+    def convert_configs(self):
         for env_mapping in self.env_mappings:
-            environment_names = env_mapping["names"]
+            convert_from = env_mapping.get("convertFrom", self._ConverterType.Generic.name)
+            convert_to = env_mapping.get("convertTo", self._ConverterType.Generic.name)
+            names = env_mapping["names"]
             path_mappings = env_mapping["path"]
 
-            for environment in environment_names:
-                output_folder = get_output_folder_path_func(environment)
+            for environment in names:
+                output_folder = self.__get_output_folder_path(environment, convert_to)
                 os.makedirs(output_folder, exist_ok=True)
 
                 for path_filename in path_mappings:
                     path, filename = path_filename
                     with open(path) as file:
                         data = file.read()
-                        converted_data = convert_func(data, environment)
+                        converted_data = self.__convert(data, environment, convert_from, convert_to)
 
-                    output_file_name = os.path.join(output_folder, f"{environment}_{filename}{file_extension}")
+                    output_file_name = os.path.join(output_folder, f"{environment}_{filename}.{convert_to}")
 
                     with open(output_file_name, "w") as output_file:
-                        output_file.write(converted_data)
+                        output_file.write(str(converted_data))  # Convert to string explicitly
+
+    def __get_output_folder_path(self, environment, convert_to):
+        if self._WriteMode[self.write_mode] == self._WriteMode.CreateNew:
+            timestamp = str(int(time.time()))
+            return f"{self.folder_name}_{timestamp}/{environment}_environment/{convert_to}"
+        else:
+            return f"{self.folder_name}/{environment}_environment/{convert_to}"
+
+    def __convert(self, data, environment, convert_from, convert_to):
+        if convert_from == self._ConverterType.Local_Settings_FunctionApp.name and convert_to == self._ConverterType.Azure_DevOps_Release_Pipeline_FunctionApp.name:
+            return self.__convert_local_to_pipeline(data, environment)
+        elif convert_from == self._ConverterType.Azure_DevOps_Release_Pipeline_FunctionApp.name and convert_to == self._ConverterType.Azure_FunctionApp.name:
+            return self.__convert_pipeline_to_azure_fa_config(data, environment)
+        # Add more converters here for other conversion types
+        elif convert_from == self._ConverterType.Generic.name:
+            return self.__convert_generic(data, environment)
+        else:
+            raise ValueError("Unsupported conversion types specified")
+
+    def __convert_generic(self, data, environment):
+        # Placeholder for the generic conversion logic
+        pass
 
     def __convert_local_to_pipeline(self, data, environment):
         parsed_data = json.loads(data)
         formatted_strings = []
         for key, value in self.__sorted_items(parsed_data["Values"]):
             if key not in self.ignore_variables:
-                formatted_value = self.__replace_variables(key, value, environment, self.variables)
+                formatted_value = self.__replace_variables(key, value, environment)
                 formatted_strings.append(f'-{key} "{formatted_value}"')
         formatted_output = " ".join(formatted_strings)
         return formatted_output
@@ -85,17 +86,17 @@ class Converter:
         result = []
         for key, value in self.__sorted_items(values):
             if key not in self.ignore_variables:
-                formatted_value = self.__replace_variables(key, value, environment, self.variables)
+                formatted_value = self.__replace_variables(key, value, environment)
                 result.append({"name": key, "value": formatted_value, "slotSetting": False})
         converted_data = json.dumps(result, indent=2)
         return converted_data
 
-    def __replace_variables(self, key, value, environment, variables):
-        if environment in variables and key in variables[environment]:
-            value = variables[environment][key]
+    def __replace_variables(self, key, value, environment):
+        if environment in self.variables and key in self.variables[environment]:
+            value = self.variables[environment][key]
             if value.startswith("#$") and value.endswith("$#"):
                 extracted_value = value[2:-2]
-                value = variables[extracted_value][key]
+                value = self.variables[extracted_value][key]
             return value
         else:
             return value
@@ -116,8 +117,7 @@ class Converter:
 
 def main():
     converter = Converter()
-    converter.local_to_pipeline()
-    converter.pipeline_to_azure_fa_config()
+    converter.convert_configs()
 
 
 if __name__ == "__main__":
